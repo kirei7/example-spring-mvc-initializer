60e8ceac04bbb00c254e032e27283d66f638fbb7
Vladyslav Sereda <vladyslav_sereda1@epam.com>
2019-04-15 21:06:00 +0300

    Add view resolver

--- src/main/java/com/epam/rd/webmvc/config/WebMvcConfig.java
+++ src/main/java/com/epam/rd/webmvc/config/WebMvcConfig.java
@@ -1,13 +1,45 @@
 package com.epam.rd.webmvc.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import javax.servlet.ServletContext;
 
 @EnableWebMvc
 @Configuration
 class WebMvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private ServletContext servletContext;
+
+    @Bean
+    public ServletContextTemplateResolver templateResolver() {
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+        templateResolver.setPrefix("WEB-INF/classes/view/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML5");
+
+        return templateResolver;
+    }
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        return templateEngine;
+    }
+    @Bean
+    public ThymeleafViewResolver viewResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setOrder(1);
+        return viewResolver;
+    }
 
 }
--- src/main/java/com/epam/rd/webmvc/controller/IndexController.java
+++ src/main/java/com/epam/rd/webmvc/controller/IndexController.java
@@ -1,18 +1,21 @@
 package com.epam.rd.webmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping(value = "/")
 class IndexController {
 
     @SuppressWarnings("SameReturnValue")
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    @ResponseBody
-    public String showIndex() {
-        return "Hello world";
+    @GetMapping
+    public String showIndex(Model model) {
+        model.addAttribute("user", "John");
+        return "index";
     }
 
 }
